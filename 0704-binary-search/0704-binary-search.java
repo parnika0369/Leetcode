95407110b88dd08d81fd3e02ceed50646a3dc75d
Parnika Sharma <parnika03066030@gmail.com>
2026-08-16 20:31:57 +0530

    Time: 0 ms (100%), Space: 48.5 MB (14.3%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,3 +1,4 @@
+ /*
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
@@ -20,3 +21,38 @@ class Solution {
         return -1;
     }
 }
+
+with recursion
+ */
+
+ class Solution{
+    public int search(int[] nums,int target){
+        int n= nums.length;
+        int low =0;
+        int high =n-1;
+        return helper(nums,target,low,high);
+
+    }
+    private int helper(int[] nums,int target,int low,int high){
+        if(low>high){
+            return -1;
+        }
+        int mid = (low+high)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(nums[mid]<target){
+            return helper(nums,target,mid+1,high);
+        }
+        else{
+            return helper(nums,target,low,mid-1);
+        }
+
+    }
+ }
+
+
+
+
+
+
