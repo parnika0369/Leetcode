9228f402ded6a563bf52abf4ca015e3d43512e88
Parnika Sharma <parnika03066030@gmail.com>
2026-09-03 10:30:36 +0530

    Time: 0 ms (100%), Space: 42.8 MB (37.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,40 @@
+// class Solution {
+//     private int chori(int[] nums,int i,int[] dp){
+//         if(i>=nums.length){
+//             return 0;
+//         }
+//         if(dp[i] != -1){
+//             return dp[i];
+//         }
+//         int choriFrom0 = nums[i] + chori(nums,i+2,dp);
+//         int choriFrom1 = chori(nums,i+1,dp);
+//         return dp[i] =  Math.max(choriFrom0,choriFrom1);
+//     }
+//     public int rob(int[] nums) {
+//         int i = 0;
+//         int[] dp = new int[nums.length];
+//         Arrays.fill(dp,-1);
+//         return chori(nums,i,dp);
+//     }
+// }
+
+
+//tabulation
 class Solution {
-    private int chori(int[] nums,int i,int[] dp){
-        if(i>=nums.length){
-            return 0;
-        }
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        int choriFrom0 = nums[i] + chori(nums,i+2,dp);
-        int choriFrom1 = chori(nums,i+1,dp);
-        return dp[i] =  Math.max(choriFrom0,choriFrom1);
-    }
     public int rob(int[] nums) {
-        int i = 0;
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return chori(nums,i,dp);
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[n];
+        // Base cases
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            int choriFrom0 = nums[i] + dp[i - 2];
+            int choriFrom1 = dp[i - 1];
+            dp[i] = Math.max(choriFrom0, choriFrom1);
+        }
+        return dp[n - 1];
     }
 }
-
