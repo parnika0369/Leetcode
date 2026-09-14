bbf3c8458c39619fdc019e7b1499d27d80140a42
Parnika Sharma <parnika03066030@gmail.com>
2026-09-14 09:57:39 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.7%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,17 @@
+//space optimiation!
 class Solution {
     public int climbStairs(int n) {
-        Integer[] dp = new Integer[n+1];
-        dp[0]=1;
-        dp[1]=1;
+        // Integer[] dp = new Integer[n+1];
+        // dp[0]=1;
+        // dp[1]=1;
+        int dp0 = 1;
+        int dp1 =1;
+        int dpn = 0;
         for(int i=2;i<n+1;i++){
-            dp[i]= dp[i-1]+dp[i-2];
+            dpn = dp0;
+            dp0 = dp1;
+            dp1= dpn+dp0;
         }
-        return dp[n];
+        return dp1;
     }
 }
