3a678d1c1bccbfac9511b82e717a6c1a382d16cd
Parnika Sharma <parnika03066030@gmail.com>
2026-09-14 16:10:35 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.49%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,40 +1,15 @@
-// class Solution {
-//     private int chori(int[] nums,int i,int[] dp){
-//         if(i>=nums.length){
-//             return 0;
-//         }
-//         if(dp[i] != -1){
-//             return dp[i];
-//         }
-//         int choriFrom0 = nums[i] + chori(nums,i+2,dp);
-//         int choriFrom1 = chori(nums,i+1,dp);
-//         return dp[i] =  Math.max(choriFrom0,choriFrom1);
-//     }
-//     public int rob(int[] nums) {
-//         int i = 0;
-//         int[] dp = new int[nums.length];
-//         Arrays.fill(dp,-1);
-//         return chori(nums,i,dp);
-//     }
-// }
-
-
-//tabulation
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) {
+        if(n==1){
             return nums[0];
         }
-        int[] dp = new int[n];
-        // Base cases
+        Integer[] dp = new Integer[n+1];
         dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < n; i++) {
-            int choriFrom0 = nums[i] + dp[i - 2];
-            int choriFrom1 = dp[i - 1];
-            dp[i] = Math.max(choriFrom0, choriFrom1);
+        dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max((nums[i]+dp[i-2]),dp[i-1]);
         }
-        return dp[n - 1];
+        return dp[n-1];
     }
 }
