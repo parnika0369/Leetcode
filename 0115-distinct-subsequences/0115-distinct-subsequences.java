24c5d61f6a9b5e490451b67b94e4137cbce53b4e
Parnika Sharma <parnika03066030@gmail.com>
2026-09-16 09:56:11 +0530

    Time: 32 ms (5.48%), Space: 79.9 MB (5.02%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,22 +1,47 @@
-class Solution {
-    private int helper(String s,String t,int i,int j , Integer[][] dp){
-        if(j==0)return 1;
-        if(i==0)return 0;
+// class Solution {
+//     private int helper(String s,String t,int i,int j , Integer[][] dp){
+//         if(j==0)return 1;
+//         if(i==0)return 0;
         
-        if(dp[i][j]!=null)return dp[i][j];
-        if(s.charAt(i-1)==t.charAt(j-1)){
-            dp[i][j]= helper(s,t,i-1,j,dp)+helper(s,t,i-1,j-1,dp);
-        }
-        else{
-            dp[i][j] = helper(s,t,i-1,j,dp);
-        }
-        return dp[i][j];
+//         if(dp[i][j]!=null)return dp[i][j];
+//         if(s.charAt(i-1)==t.charAt(j-1)){
+//             dp[i][j]= helper(s,t,i-1,j,dp)+helper(s,t,i-1,j-1,dp);
+//         }
+//         else{
+//             dp[i][j] = helper(s,t,i-1,j,dp);
+//         }
+//         return dp[i][j];
 
-    }
+//     }
+//     public int numDistinct(String s, String t) {
+//         int n = s.length();
+//         int m = t.length();
+//         Integer[][] dp = new Integer[n+1][m+1];
+//         return helper(s,t,n,m,dp);
+//     }
+// }
+
+class Solution{
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
         Integer[][] dp = new Integer[n+1][m+1];
-        return helper(s,t,n,m,dp);
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        for(int j=1;j<=m;j++){
+            dp[0][j] = 0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j]+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]= dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
