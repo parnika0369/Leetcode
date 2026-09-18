75888f2fbdb9f170039174c0843ad802db385fe0
Parnika Sharma <parnika03066030@gmail.com>
2026-09-18 17:41:29 +0530

    Time: 0 ms (100%), Space: 42.7 MB (77.58%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,17 +1,21 @@
+import java.util.Arrays;
 class Solution {
+    private int helper(int[] nums,int start ,int i,Integer[] dp){
+        if(i<start) return 0;
+        if(dp[i]!= null) return dp[i];
+        int chori1 = nums[i]+helper(nums,start,i-2,dp);
+        int chori2 = helper(nums,start,i-1,dp);
+        dp[i] = Math.max(chori1,chori2);
+        return dp[i];
+
+    }
     public int rob(int[] nums) {
         int n = nums.length;
-        if (n == 1) return nums[0];
-        return Math.max(robLinear(nums, 0, n - 2), robLinear(nums, 1, n - 1));
-    }
-
-    private int robLinear(int[] nums, int start, int end) {
-        int prev2 = 0, prev1 = 0;
-        for (int i = start; i <= end; i++) {
-            int curr = Math.max(nums[i] + prev2, prev1);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
+        if(n==1) return nums[0];
+        Integer[] dp = new Integer[n+1];
+        int a = helper(nums,0,n-2,dp);
+        Arrays.fill(dp,null);
+        int b = helper(nums,1,n-1,dp);
+        return Math.max(a,b);
     }
 }
