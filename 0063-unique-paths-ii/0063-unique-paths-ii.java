7e3569837103a261b91541f5c346b58e4e8d4a51
Parnika Sharma <parnika03066030@gmail.com>
2026-09-19 13:48:44 +0530

    Time: 0 ms (100%), Space: 43.3 MB (90.93%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,17 +1,25 @@
 class Solution {
-    private int helper(int i, int j, Integer[][] dp, int[][] obstacleGrid) {
-        if (i < 0 || j < 0) return 0;
-        if (obstacleGrid[i][j] == 1) return 0;
-        if (i == 0 && j == 0) return 1;
-        if (dp[i][j] != null) return dp[i][j];
-        int down = helper(i - 1, j, dp, obstacleGrid);
-        int right = helper(i, j - 1, dp, obstacleGrid);
-        return dp[i][j] = down + right; 
+    private int helper(int[][] obstacleGrid,int i,int j,Integer[][] dp){
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        
+        if(i==m) return 0;
+        if(j==n)return 0;
+        if(obstacleGrid[i][j]==1){
+            return 0;
+        }
+        if(i==m-1&&j==n-1)return 1;
+        if(dp[i][j]!= null) return dp[i][j];
+        
+        int down = helper(obstacleGrid,i+1,j,dp);
+        int right = helper(obstacleGrid,i,j+1,dp);
+        dp[i][j] = down + right;
+        return dp[i][j];
     }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
         Integer[][] dp = new Integer[m][n];
-        return helper(m - 1, n - 1, dp, obstacleGrid);
+        return helper(obstacleGrid,0,0,dp);
     }
 }
